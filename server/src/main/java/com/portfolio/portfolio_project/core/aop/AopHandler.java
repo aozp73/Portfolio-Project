65eac6725b5b059c252510caa0d4181d3f8365a5
aozp73 <aozp73@naver.com>
2023-10-29 16:11:57 +0900

    AOP 로직 처리 - 페이지 요청 시, Sentry 메시지 전송

---
+++ server/src/main/java/com/portfolio/portfolio_project/core/aop/AopHandler.java
@@ -0,0 +1,24 @@
+package com.portfolio.portfolio_project.core.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import io.sentry.Sentry;
+
+@Aspect
+@Component
+public class AopHandler {
+    
+    @Pointcut("@annotation(com.portfolio.portfolio_project.core.aop.SentryMonitoring)")
+    public void sentryMonitoring() {
+    }
+
+    @Before("sentryMonitoring()")
+    public void sentryMonitoring(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        Sentry.captureMessage(methodName + " 요청이 응답되었습니다.");
+    }
+}
