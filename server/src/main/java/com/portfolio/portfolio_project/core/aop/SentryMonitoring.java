773906ff747a90cd6c7ee6f266bbf1a9fc497780
aozp73 <aozp73@naver.com>
2023-10-29 16:00:57 +0900

    애너테이션 생성 - 요청 확인 모니터링 용도

---
+++ server/src/main/java/com/portfolio/portfolio_project/core/aop/SentryMonitoring.java
@@ -0,0 +1,14 @@
+package com.portfolio.portfolio_project.core.aop;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SentryMonitoring {
+    
+}
